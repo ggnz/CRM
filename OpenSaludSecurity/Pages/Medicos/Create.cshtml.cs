5ae730bfbe3b957a9ded454df26f4a4a29aa3d6f
Jorge Grimaldi <jgrimaldi@microsoft.com>
2022-03-07 19:28:57 -0600

    Revisar que solo se puedan crear medicos en las clinicas de las cuales el usuario es representante

--- OpenSaludSecurity/Pages/Medicos/Create.cshtml.cs
+++ OpenSaludSecurity/Pages/Medicos/Create.cshtml.cs
@@ -2,32 +2,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using OpenSaludSecurity.Data;
 using OpenSaludSecurity.Models;
+using OpenSaludSecurity.Pages.Shared;
 
 namespace OpenSaludSecurity.Pages.Medicos
 {
-    public class CreateModel : PageModel
+    public class CreateModel : _BasePageModel
     {
         private readonly OpenSaludSecurity.Data.ApplicationDbContext _context;
 
-        public CreateModel(OpenSaludSecurity.Data.ApplicationDbContext context)
+        public CreateModel(ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager)
+            : base(context, authorizationService, userManager)
         {
-            _context = context;
         }
 
-        public IActionResult OnGet()
+        public async Task OnGetAsync()
         {
-            return Page();
+            // Calcular el ClinicaRefId al que pertenece el representante que abrio la pagina de create
+            var clinicas = from c in Context.Clinica
+                          select c;
+
+            Clinicas =  await clinicas.ToListAsync();
+
+            UserId = UserManager.GetUserId(User);
+
+            IdClinicasDisponibles = new List<SelectListItem>();
+            foreach (Clinica c in Clinicas)
+            {
+                if (c.IdRepresentante == UserId)
+                {
+                    IdClinicasDisponibles.Add(new SelectListItem { Value = c.IdClinica.ToString(), Text = c.Nombre});
+                }
+            }
         }
 
         [BindProperty]
         public Medico Medico { get; set; }
 
         public int ClinicaRefId { get; set; }
+
+        public IList<Clinica> Clinicas { get; set; }
+
+        public List<SelectListItem> IdClinicasDisponibles { get; set; }
+
+        public string UserId { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
@@ -37,8 +64,8 @@ namespace OpenSaludSecurity.Pages.Medicos
                 return Page();
             }
 
-            _context.Medico.Add(Medico);
-            await _context.SaveChangesAsync();
+            Context.Medico.Add(Medico);
+            await Context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
