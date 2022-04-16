49b724a1d7669e60f6872ffd07adb7f5931c1fc7
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-04-16 10:09:40 -0600

    startup

--- OpenSaludSecurity/Startup.cs
+++ OpenSaludSecurity/Startup.cs
@@ -13,9 +13,6 @@ using OpenSaludSecurity.Authorization;
 using OpenSaludSecurity.Data;
 using OpenSaludSecurity.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OpenSaludSecurity
 {
