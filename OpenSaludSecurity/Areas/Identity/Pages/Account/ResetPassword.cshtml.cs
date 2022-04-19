e7c0e2511fe2df268550026dac457a916a25e1e6
BrainerCR <63078436+BrainerCR@users.noreply.github.com>
2022-04-18 20:28:47 -0600

    mas traducciones

--- OpenSaludSecurity/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ OpenSaludSecurity/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -27,18 +27,20 @@ namespace OpenSaludSecurity.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "Por favor digite su correo")]
             [EmailAddress]
+            [Display(Name = "Correo")]
             public string Email { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "Por favor digite su contraseña")]
+            [Display(Name = "Contraseña")]
+            [StringLength(100, ErrorMessage = "El {0} debe tener almenos {2} y un máximo de {1} caracteres.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Confirmar Contraseña")]
+            [Compare("Password", ErrorMessage = "La contraseña y la contraseña de confirmación no coinciden.")]
             public string ConfirmPassword { get; set; }
 
             public string Code { get; set; }
